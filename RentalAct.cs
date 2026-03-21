44fa284aa13888cb25ed25a94a1aae2a4f7dc944
Artem Kraiushkin <s31384@pjwstk.edu.pl>
2026-03-21 23:39:18 +0200

    Defiend fields and implemented constructor for RentalAct class

--- RentalAct.cs
+++ RentalAct.cs
@@ -2,5 +2,30 @@
 
 public class RentalAct
 {
+    public int DeviceId { get; set; }
+    public int UserId { get; set; }
+    public DateTime RentalStartDate { get; set; }
+    public DateTime RentalEndDate { get; set; }
+    public bool ReturnedInTime { get; set; }
+    public DateTime? ReturnDate
+    {
+        get;
+        set
+        {
+            ReturnDate =  value;
+            if ( ReturnDate > RentalEndDate)
+            {
+                ReturnedInTime = false;
+            }else ReturnedInTime = true;
+        }
+    }
+
+    public RentalAct(int deviceId, int userId, DateTime rentalStartDate, DateTime rentalEndDate)
+    {
+        this.DeviceId = deviceId;
+        this.UserId = userId;   
+        this.RentalStartDate = rentalStartDate;
+        this.RentalEndDate = rentalEndDate;
+    }
     
 }
