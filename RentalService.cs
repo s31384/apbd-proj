3d2f2e2fce1fc78f586476f8cb8c4a774ccddf42
Artem Kraiushkin <s31384@pjwstk.edu.pl>
2026-03-22 01:11:25 +0200

    Implemented method returnDevice for Rental service class. Added message for rentDevice method. rentDevice method now checks if user is banned

--- RentalService.cs
+++ RentalService.cs
@@ -28,6 +28,11 @@ public class RentalService
             if (!IsDeviceAvailaible(DeviceID))
             {throw new Exception("Device is not availaible");
             }
+
+            if (!UserService.IsUserCanRent(UserID))
+            {
+                throw new Exception("User is banned");
+            }
             DateTime dateFrom;
             DateTime dateTo;
             try
@@ -57,6 +62,37 @@ public class RentalService
             devices.Find(d => d.Id == DeviceID).IsAvailable = false;
             rentalActs.Add(new RentalAct(UserID, DeviceID, dateFrom, dateTo));
             UserService.IncreaseRentAct(UserID);
+            Console.WriteLine($"{DeviceID} has been rented by {UserID}");
+    }
+    public void returnDevice(int UserID, int DeviceID, string returnDateString)
+    {
+        if (!rentalActs.Any(x => x.UserId == UserID && x.DeviceId == DeviceID))
+        {
+            throw new Exception("No rental acts found from this user or with this device");
+        }
+        RentalAct rentalAct = rentalActs.Find(x => x.UserId == UserID && x.DeviceId == DeviceID);
+
+        DateTime ReturnDate;
+        try
+        {
+            ReturnDate = DateTime.Parse(returnDateString);
+        }catch(FormatException)
+        {
+            throw new Exception("Invalid date");
+        }
+
+        if (ReturnDate < rentalAct.RentalStartDate)
+        {
+            throw new Exception("Rental start date must cant be before the rental start date");
+        }
+        rentalAct.ReturnDate = ReturnDate;
+        if (!rentalAct.ReturnedInTime)
+        {
+            UserService.PunishUser(UserID);
+        }
+        UserService.DecreaseRentAct(UserID);
+        devices.Find(x => x.Id == DeviceID).IsAvailable=true;
+        rentalActs.Remove(rentalAct);
     }
 
     
