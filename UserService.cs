8481e0362e4e56bc6e162735aa610eb3d0cae215
Artem Kraiushkin <s31384@pjwstk.edu.pl>
2026-03-21 23:18:50 +0200

    Implemented UserServie.cs

---
+++ UserService.cs
@@ -0,0 +1,22 @@
+﻿namespace apbd_proj_3;
+
+public class UserService
+{
+    private List<Person> users;
+
+    public UserService()
+    {
+        users = new List<Person>();
+
+    }
+
+    public void addPerson(Person person)
+    {
+        users.Add(person);
+    }
+
+    public bool isThisUserExists(int id)
+    {
+        return users.Any(x => x.Id == id);
+    }
+}
